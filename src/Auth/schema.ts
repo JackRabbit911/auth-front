5d8237b27436a0dfd14a44bbeda3c07ebbd3ad67
alx <world.zay@gmail.com>
2026-02-16 08:52:24 +0300

    #3 Create schema authData

---
+++ src/Auth/schema.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const authData = z.object({
+    email: z.email().trim()
+    .min(6, { message: 'Required' }),
+    password: z.string().min(5, "Password must be at least 5 characters"),
+})
+
+export type AuthData = z.infer<typeof authData>
