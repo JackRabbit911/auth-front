0e63a07e2e38f9c2d1380d7b408b22bdb85835f5
alx <world.zay@gmail.com>
2026-02-16 15:19:07 +0300

    #3 Update password schema

--- src/Auth/schema.ts
+++ src/Auth/schema.ts
@@ -3,7 +3,7 @@ import * as z from "zod"
 export const authData = z.object({
     email: z.email().trim()
         .min(6, { message: 'Required' }),
-    password: z.string().min(5, "Password must be at least 5 characters"),
+    password: z.string().min(1, "Required"),
     remember: z.boolean(),
 })
 
