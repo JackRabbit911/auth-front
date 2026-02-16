b59c2e232924d58f787a0465f7a14f8a01108861
alx <world.zay@gmail.com>
2026-02-16 12:11:16 +0300

    #3 Add remember field to authData schema

--- src/Auth/schema.ts
+++ src/Auth/schema.ts
@@ -2,8 +2,9 @@ import * as z from "zod"
 
 export const authData = z.object({
     email: z.email().trim()
-    .min(6, { message: 'Required' }),
+        .min(6, { message: 'Required' }),
     password: z.string().min(5, "Password must be at least 5 characters"),
+    remember: z.boolean(),
 })
 
 export type AuthData = z.infer<typeof authData>
