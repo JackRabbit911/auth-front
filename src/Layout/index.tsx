89e3ea38db2dd26a30c7edca1b6946042728a7bd
alx <world.zay@gmail.com>
2026-02-16 12:13:16 +0300

    #3 Update template

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -4,12 +4,11 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="flex flex-row justify-center">
-      <div className="w-full md:w-xl lg:w-3xl bg-base-100 p-4">
-        <div className="flex flex-row justify-between mb-4">
-          <h1 className="text-2xl">It`s work!</h1>
+    <div className="flex flex-col justify-center min-h-[84vh]">
+      <div className="flex flex-row justify-center">
+        <div className="w-full md:w-xl lg:w-3xl h-full bg-base-300 border border-zinc-600 rounded-sm p-4">
+          {children}
         </div>
-        {children}
       </div>
     </div>
   )
