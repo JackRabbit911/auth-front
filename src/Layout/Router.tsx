ede26e2c096669ea338950269289acca10ef00f1
alx <world.zay@gmail.com>
2026-02-18 19:55:14 +0300

    #7 Add recovery/alert route

--- src/Layout/Router.tsx
+++ src/Layout/Router.tsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router"
 import Auth from "Auth"
 import Restore from "Restore"
 import Register from "Register"
+import AlertInfo from "Restore/AlertInfo"
 
 const Router = () => {
   return (
     <Routes>
       <Route path='' element={<Auth />} />
       <Route path='register' element={<Register />} />
-      <Route path='restore' element={<Restore />} />
+      <Route path='recovery'>
+        <Route path='email' element={<Restore />} />
+        <Route path='alert' element={<AlertInfo />} />
+      </Route>
     </Routes>
   )
 }
