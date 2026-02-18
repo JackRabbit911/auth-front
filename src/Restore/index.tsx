b8d37041433e3435c27edfb9c78a880279bfb336
alx <world.zay@gmail.com>
2026-02-18 22:26:54 +0300

    #7 Add form and apply useEmailForm custom hook

--- src/Restore/index.tsx
+++ src/Restore/index.tsx
@@ -1,7 +1,31 @@
+import { FormProvider } from "react-hook-form"
+
+import TextInput from "reused/TextInput"
+import { useEmailForm } from "./hooks/emailForm";
+
 const Restore = () => {
-    return (
-        <>Restore</>
-    )
+  const { methods, onSubmit, disabled } = useEmailForm()
+  
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <div className="flex flex-row justify-center">
+          <h1 className="text-xl">{'Password recovery'}</h1>
+        </div>
+        <TextInput
+          fieldName="email"
+          label="Email"
+          optional="Enter the email you provided"
+        />
+        <button
+          className="btn btn-primary dark:btn-info w-full my-4"
+          disabled={disabled}
+        >
+          Send
+        </button>
+      </form>
+    </FormProvider>
+  )
 }
 
 export default Restore
