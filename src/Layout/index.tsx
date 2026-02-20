ac73b126b149d904acf66dfd1a53b6c7b012342b
alx <world.zay@gmail.com>
2026-02-20 12:27:31 +0300

    #7 Update Layput template

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col justify-center min-h-[84vh]">
       <div className="flex flex-row justify-center">
-        <div className="w-full md:w-xl lg:w-3xl h-full bg-base-300 border border-zinc-600 rounded-sm p-4">
+        <div className="w-full md:w-lg lg:w-xl h-full bg-base-300 border border-zinc-600 rounded-sm p-4">
           {children}
         </div>
       </div>
