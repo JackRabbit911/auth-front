811b9568d92f4d7dcd10990e65e7849a0245fb3a
alx <world.zay@gmail.com>
2026-02-20 17:01:03 +0300

    #7 Add confirmValidationError schema and infer it`s type

--- src/Restore/schema.ts
+++ src/Restore/schema.ts
@@ -12,7 +12,13 @@ const serverValidationError = z.object({
 
 export const serverValidationErrors = z.array(serverValidationError)
 
+const confirmValidationError = z.object({
+    key: z.literal('confirmPassword'),
+    msg: z.string(),
+})
+
 export const passwordSchema = z.object({
+  id: z.number(),
   password: z.string().min(5, "Password must be at least 5 characters"),
   confirmPassword: z.string(),
 }).refine((data) => data.password === data.confirmPassword, {
@@ -23,3 +29,4 @@ export const passwordSchema = z.object({
 export type Email = z.infer<typeof emailSch>
 export type ServerValidationError = z.infer<typeof serverValidationError>
 export type ConfirmPassword = z.infer<typeof passwordSchema>
+export type ConfirmValidationError = z.infer<typeof confirmValidationError>
