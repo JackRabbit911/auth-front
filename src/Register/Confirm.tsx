30074a2c4cc75caa1a5322753df60357051d7621
alx <world.zay@gmail.com>
2026-02-24 16:00:38 +0300

    #11 Create Confirm component

---
+++ src/Register/Confirm.tsx
@@ -0,0 +1,29 @@
+import ajax from "common/ajax"
+import { confirmUri } from "common/constants"
+import { useEffect } from "react"
+import { useNavigate, useParams } from "react-router"
+
+const Confirm = () => {
+  const { code } = useParams()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const uri = [confirmUri, code].join('/')
+    ajax.get(uri).then((response) => response.data)
+      .then((data) => {
+        if (data.success) {
+          if (data.result) {
+            navigate('/')
+          } else {
+            navigate('/register/alert/warning')
+          }
+        } else {
+          console.log(data)
+        }
+      })
+  }, [])
+
+  return <>Wait, please...</>
+}
+
+export default Confirm
