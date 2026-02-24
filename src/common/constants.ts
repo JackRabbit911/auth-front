4658b829e1d8a57b773a6c7f5d88b763e1d4700a
alx <world.zay@gmail.com>
2026-02-24 16:02:36 +0300

    #11 Add and update 5 endpoints as constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,7 +1,9 @@
 export const loginUri = 'auth/login'
 export const getReferenceUri = 'auth'
-export const emailCheckUri = 'auth/email'
-export const passwordCheckUri = 'auth/confirm'
-export const passwordSaveUri = 'register/savepswd'
+export const emailCheckUri = 'recovery/email'
+export const passwordCheckUri = 'recovery/confirm'
+export const passwordSaveUri = 'recovery/savepswd'
+export const registerUri = 'register/save'
+export const confirmUri = 'register/confirm'
 
 export const getTranslateUri = '/gettranslate'
