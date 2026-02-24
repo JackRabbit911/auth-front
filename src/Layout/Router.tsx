e98bacaa04063de617401d56340df3c6c95ae517
alx <world.zay@gmail.com>
2026-02-24 16:06:14 +0300

    #11 Add register children routes

--- src/Layout/Router.tsx
+++ src/Layout/Router.tsx
@@ -6,12 +6,22 @@ import AlertInfo from "Restore/AlertInfo"
 import Password from "Restore/Password"
 import AlertWarning from "Restore/AlertWarning"
 import AlertSuccess from "Restore/AlertSuccess"
+import RegisterInfo from "Register/alert/RegisterInfo"
+import Confirm from "Register/Confirm"
+import RegisterWarning from "Register/alert/RegisterWarning"
 
 const Router = () => {
   return (
     <Routes>
       <Route path='' element={<Auth />} />
-      <Route path='register' element={<Register />} />
+      <Route path='register'>
+        <Route path='' element={<Register />} />
+        <Route path='alert'>
+          <Route path='info' element={<RegisterInfo />} />
+          <Route path='warning' element={<RegisterWarning />} />
+        </Route>
+        <Route path='confirm/:code' element={<Confirm />} />
+      </Route>
       <Route path='recovery'>
         <Route path='email' element={<Restore />} />
         <Route path='alert'>
