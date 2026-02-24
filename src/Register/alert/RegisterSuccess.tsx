e00883628aaf621d8940d01c4e6ce4577e41a054
alx <world.zay@gmail.com>
2026-02-24 18:15:52 +0300

    #11 Create RegisterSuccess component

---
+++ src/Register/alert/RegisterSuccess.tsx
@@ -0,0 +1,31 @@
+import { useTranslate } from "common/i18n/hook";
+import { __ } from "common/i18n/utils";
+import { Link } from "react-router";
+import Alert from "reused/Alert";
+import Success from "reused/icons/Success";
+
+const RegisterSuccess = () => {
+  useTranslate()
+  
+  return (
+    <Alert
+      className="alert alert-success"
+      icon={<Success />}
+    >
+      <h3 className="text-lg font-medium">
+        {__('Congratulations!')}
+      </h3>
+      <p>
+        Вы зарегистрированы на сайте buri.me
+      </p>
+      <Link to="/">
+        <button className="btn w-full mt-4">
+          {__('Welcome to Sign In!')}
+        </button>
+      </Link>
+    </Alert>
+
+  )
+}
+
+export default RegisterSuccess
