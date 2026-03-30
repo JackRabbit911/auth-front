9a792539922c04d3bdb4418918719ed328f7a235
alx <world.zay@gmail.com>
2026-03-30 16:08:19 +0300

    #25 Add necessary types

--- src/common/i18n/types.ts
+++ src/common/i18n/types.ts
@@ -1,4 +1,19 @@
 type StrNum = string|number;
-
 export type Argv = StrNum[];
 export type GetText = (value: string, ...argv: Argv) => string;
+
+export type TranslateType = {
+  [key: string]: string;
+}
+
+export type TranslateContextType = {
+  gettext: GetText;
+  setTranslate: React.Dispatch<React.SetStateAction<TranslateType>>;
+  lang: string;
+  setLang: React.Dispatch<React.SetStateAction<string>>;
+}
+
+type SetLang = (lang: string) => void;
+export type SetLangHookType = [lang: string, setLang: SetLang];
+
+export type FetchTranslateType = (lang: string, keys: string[]) => Promise<TranslateType>
