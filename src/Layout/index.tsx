3c9f85826eba0e9b16947aaa7c047bc7eca44efe
alx <world.zay@gmail.com>
2026-03-30 15:08:23 +0300

    #23 Add error and loading handling

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,14 +1,22 @@
+import ErrorCmp from "reused/ErrorCmp";
+import Loading from "reused/Loading";
+import { useAppSelector } from "store/hooks";
+
 type Props = {
   children?: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
+  const { loading, error } = useAppSelector((state) => state.common)
+
   return (
     <div className="flex flex-col justify-center min-h-[84vh]">
       <div className="flex flex-row justify-center">
-        <div className="w-full md:w-lg lg:w-xl h-full bg-base-300 border border-zinc-600 rounded-sm p-4">
-          {children}
-        </div>
+        {loading ? <Loading /> : (error ? <ErrorCmp status={error} /> :
+          <div className="w-full md:w-lg lg:w-xl h-full bg-base-300 border border-zinc-600 rounded-sm p-4">
+            {children}
+          </div>
+        )}
       </div>
     </div>
   )
