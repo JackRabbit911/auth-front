ee02533a4685c9c19d0962fc5418b70330a875f8
alx <world.zay@gmail.com>
2026-03-30 16:35:28 +0300

    #25 Apply BrowserRouter wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import store from "store"
+import { BrowserRouter } from 'react-router'
+
+const prefix = () => {
+  const lang = document.querySelector('html')?.getAttribute('lang');
+  return (lang === 'ru') ? '/auth' : `/${lang}/auth`
+}
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store} >
-        <App />
+        <BrowserRouter basename={prefix()}>
+            <App />
+        </BrowserRouter>
     </Provider>
 )
