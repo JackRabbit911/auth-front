3de79a3c6c0c806d93f3f9b797c91a0a3fe25a2e
alx <world.zay@gmail.com>
2026-03-30 16:49:38 +0300

    #25 Switch detect lang logic

--- src/common/i18n/config.ts
+++ src/common/i18n/config.ts
@@ -1,5 +1,5 @@
-// import { detectLangByAttribute } from "./utils"
-import { detectLangByUri } from "./utils"
+import { detectLangByAttribute } from "./utils"
+// import { detectLangByUri } from "./utils"
 import { fetchTranslate } from "./utils"
 // import { fetchAllMap } from "./utils"
 import type { GetText, TranslateType } from "./types"
@@ -15,8 +15,8 @@ export const limit = null //cache limit in pairs key-valaue
 export const delay = 50 //debounse delay im ms
 export const getTranslateUri = 'http://localhost/api/gettranslate'
 
-// export const detectLang = (): string => detectLangByAttribute()
-export const detectLang = (): string => detectLangByUri()
+export const detectLang = (): string => detectLangByAttribute()
+// export const detectLang = (): string => detectLangByUri()
 
 //fetch translate by array keys
 export const getTranslate = (lang: string, keys: string[] | null): Promise<TranslateType> => fetchTranslate(lang, keys)
