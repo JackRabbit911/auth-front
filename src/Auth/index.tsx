298695b08f27aeb29f01f48d96123df25d285653
alx <world.zay@gmail.com>
2026-03-31 17:03:33 +0300

    #25 Apply ErrorOrPending wrapper

--- src/Auth/index.tsx
+++ src/Auth/index.tsx
@@ -5,56 +5,59 @@ import { useAuthForm } from "./hooks";
 import CheckBox from "reused/CheckBox";
 import TextInput from "reused/TextInput";
 import { useTranslate } from "common/i18n/hooks";
+import ErrorOrPending from "reused/ErrorOrPendig";
 
 const Auth = () => {
   const { methods, onSubmit, disabled } = useAuthForm()
   const __ = useTranslate()
 
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <div className="flex flex-row justify-center">
-          <h1 className="text-xl">{__('Sign in to your account')}</h1>
-        </div>
-        <TextInput
-          fieldName="email"
-          label="Email"
-        />
-        <TextInput
-          fieldName="password"
-          type="password"
-          label={__('Password')}
-          optional={__("min % symbols", 5)}
-        />
-        <div className="flex flex-row justify-between gap-4 my-4">
-          <CheckBox
-            fieldName="remember"
-            label={__('Remember me on this device')}
+    <ErrorOrPending>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <div className="flex flex-row justify-center">
+            <h1 className="text-xl">{__('Sign in to your account')}</h1>
+          </div>
+          <TextInput
+            fieldName="email"
+            label="Email"
           />
-          <div className="text-end">
-            <Link to='recovery/email'>
-              <span className="fieldset link">
-                {__('Forgot password?')}
+          <TextInput
+            fieldName="password"
+            type="password"
+            label={__('Password')}
+            optional={__("min % symbols", 5)}
+          />
+          <div className="flex flex-row justify-between gap-4 my-4">
+            <CheckBox
+              fieldName="remember"
+              label={__('Remember me on this device')}
+            />
+            <div className="text-end">
+              <Link to='recovery/email'>
+                <span className="fieldset link">
+                  {__('Forgot password?')}
+                </span>
+              </Link>
+            </div>
+          </div>
+          <button
+            className="btn btn-primary dark:btn-info w-full mb-4"
+            disabled={disabled}
+          >
+            {__('Sign In')}
+          </button>
+          <div className="fieldset flex justify-end gap-4">
+            <span>{__('No account?')}</span>
+            <Link to='/register'>
+              <span className="link font-bold">
+                {__('Sign Up, please:')}
               </span>
             </Link>
           </div>
-        </div>
-        <button
-          className="btn btn-primary dark:btn-info w-full mb-4"
-          disabled={disabled}
-        >
-          {__('Sign In')}
-        </button>
-        <div className="fieldset flex justify-end gap-4">
-          <span>{__('No account?')}</span>
-          <Link to='/register'>
-            <span className="link font-bold">
-              {__('Sign Up, please:')}
-            </span>
-          </Link>
-        </div>
-      </form>
-    </FormProvider>
+        </form>
+      </FormProvider>
+    </ErrorOrPending>
   )
 }
 
