fe6166112272c5703116eafe20377c15fcdc537f
alx <world.zay@gmail.com>
2026-03-31 17:00:26 +0300

    #25 Remove conditional rendering

--- src/Restore/Password.tsx
+++ src/Restore/Password.tsx
@@ -4,33 +4,25 @@ import { useNavigate, useParams } from "react-router";
 import PasswordForm from "./PasswordForm";
 import { getCsrfThunk } from "store/csrf";
 import { passwordCheckUri } from "common/constants";
-import { useAppDispatch, useAppSelector } from "store/hooks";
+import { useAppDispatch } from "store/hooks";
 
 const Password = () => {
   const navigate = useNavigate()
   const { id, code } = useParams()
   const dispatch = useAppDispatch()
-  const csrf = useAppSelector((state) => state.csrf.data)
 
   useEffect(() => {
-    debugger
-    if (!csrf) {
-      const uri = [passwordCheckUri, id, code].join('/')
-      dispatch(getCsrfThunk(uri)).unwrap()
-        .then((data) => {
-          if (!data.result) {
-            console.log(data)
-            navigate('/recovery/alert/warning')
-          }
-        })
-    }
+    const uri = [passwordCheckUri, id, code].join('/')
+    dispatch(getCsrfThunk(uri)).unwrap()
+      .then((data) => {
+        if (!data.result) {
+          console.log(data)
+          navigate('/recovery/alert/warning')
+        }
+      })
   }, [])
 
-  return (
-    <>
-      {!csrf ? null : <PasswordForm id={Number(id)} />}
-    </>
-  )
+  return <PasswordForm id={Number(id)} />
 }
 
 export default Password
