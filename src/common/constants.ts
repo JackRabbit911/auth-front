21fbed361dc21400cd0dc786465024a32920069e
alx <world.zay@gmail.com>
2026-04-13 19:59:08 +0300

    #38 Add ORIGIN constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,6 @@
+const loc = window.location
+export const ORIGIN = loc.port === '5173' ? 'http://localhost' : loc.origin
+
 export const loginUri = 'auth/login'
 export const emailCheckUri = 'recovery/email'
 export const passwordCheckUri = 'recovery/confirm'
