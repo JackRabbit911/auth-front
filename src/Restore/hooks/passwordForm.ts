e181936a0628abf1b84f468c042f1773c6c82949
alx <world.zay@gmail.com>
2026-05-02 18:14:39 +0300

    #49 Remove usage csrf

--- src/Restore/hooks/passwordForm.ts
+++ src/Restore/hooks/passwordForm.ts
@@ -1,17 +1,15 @@
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import { useNavigate, useParams } from "react-router";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
-import useCsrf from "Restore/hooks/useCsrf";
 import { isObjectEmpty } from "common/utils";
 import { useRestorePswdMutation } from "common/api";
 import { passwordSchema, type ConfirmPassword, type ConfirmValidationError } from "Restore/schema";
 
 export const usePasswordForm = () => {
-    const { id } = useParams()
+    const { id, code } = useParams()
     const navigate = useNavigate()
-    const csrf = useCsrf()
     const [restorePswd, { isLoading, isError, error }] = useRestorePswdMutation()
     const responseStatus = { isLoading, isError, error }
 
@@ -20,16 +18,17 @@ export const usePasswordForm = () => {
         mode: 'all',
         defaultValues: {
             id: Number(id),
+            code: code,
             password: '',
             confirmPassword: '',
-            _csrf: csrf,
         }
     });
 
     const onSubmit: SubmitHandler<ConfirmPassword> = useCallback(async (formData) => {
         const data = await restorePswd(formData).unwrap()
         if (data.success) {
-            navigate('/recovery/alert/success')
+            const link = data.result ? 'success' : 'warning'
+            navigate(`/recovery/alert/${link}`)
         } else if (Array.isArray(data?.error)) {
             data.error.forEach((item: ConfirmValidationError) => {
                 methods.setError(item.key, {
@@ -47,12 +46,6 @@ export const usePasswordForm = () => {
 
     const disabled = !isObjectEmpty(methods.formState.errors) ||
         password === '' || confirm === '' || password !== confirm
-
-    useEffect(() => {
-        if (csrf) {
-            methods.setValue('_csrf', csrf)
-        }
-    }, [csrf])
 
     return { methods, onSubmit, disabled, responseStatus }
 }
